3573131ddbc095e6633856caacaaaa3499d33b3b
NicoRiot <nriot@umoove-inc.com>
2021-05-20 17:35:59 +0200

    add componenet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { ShopComponent } from './shop/shop.component';
+import { ProductComponent } from './shop/product/product.component';
+import { ProductItemComponent } from './shop/product-item/product-item.component';
+import { ProductCartComponent } from './shop/product-cart/product-cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ContactComponent,
+    LoginComponent,
+    RegisterComponent,
+    ShopComponent,
+    ProductComponent,
+    ProductItemComponent,
+    ProductCartComponent
   ],
   imports: [
     BrowserModule
